bd56751b1a41e68d25e774c66b21db9fadf54b19
alxd <github@alxd.org>
2021-11-09 17:46:00 +0100

    first pass as google_ads mode models

--- models/google_ads_connector/stg_google_ads__account.sql
+++ models/google_ads_connector/stg_google_ads__account.sql
@@ -1,14 +1,11 @@
 {{ config(enabled=var('api_source') == 'google_ads') }}
 
 with base as (
-
     select * 
     from {{ ref('stg_google_ads__account_tmp') }}
-
 ),
 
 fields as (
-
     select
         {{
             fivetran_utils.fill_staging_columns(
@@ -16,18 +13,18 @@ fields as (
                 staging_columns=get_account_columns()
             )
         }}
-        
+        {{ fivetran_utils.add_dbt_source_relation() }}
     from base
 ),
 
 final as (
-    
     select 
         id as account_id, 
         _fivetran_synced, 
         account_label_name, 
         currency_code, 
         name as account_name
+        {{ fivetran_utils.source_relation() }}
     from fields
 )
 
