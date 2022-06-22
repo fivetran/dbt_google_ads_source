824b11f7cd10853e2a1243a38fdaebbdc6be16c7
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2022-06-22 15:03:20 -0500

    macro fix

--- macros/get_ad_stats_columns.sql
+++ macros/get_ad_stats_columns.sql
@@ -2,7 +2,7 @@
 
 {% set columns = [
     {"name": "_fivetran_id", "datatype": dbt_utils.type_string()},
-    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()}
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
     {"name": "ad_group", "datatype": dbt_utils.type_string()},
     {"name": "ad_id", "datatype": dbt_utils.type_int()},
     {"name": "ad_network_type", "datatype": dbt_utils.type_string()},
